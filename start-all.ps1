e15a717ea7d6a6f3418ab6d45724667e448b5725
styx0r <christoph.baldow@gmail.com>
2026-05-06 21:23:26 +0200

    feat: add MinIO environment variables to start-all.ps1 for enhanced storage configuration in development

--- start-all.ps1
+++ start-all.ps1
@@ -1,5 +1,8 @@
 $env:BACKEND_URL = "http://192.168.96.170:3333"
 $env:NEXT_PUBLIC_BACKEND_URL = "http://192.168.96.170:3333"
+$env:MINIO_ENDPOINT = "192.168.96.170"
+$env:MINIO_PORT = "9000"
+$env:MINIO_USE_SSL = "false"
 $env:NX_SKIP_NX_CACHE = "true"
 
 npm run start:all
